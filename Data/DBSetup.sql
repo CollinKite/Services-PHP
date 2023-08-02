df4531a146b4649ed109e43202e9a455648b7346
Collin Kite <me@collinkite.com>
2023-08-01 19:50:52 -0600

    Fixed DB Stuff

--- Data/DBSetup.sql
+++ Data/DBSetup.sql
@@ -1,8 +1,45 @@
+DROP TABLE IF EXISTS `Admin`;
+DROP TABLE IF EXISTS `Customer`;
+DROP TABLE IF EXISTS `Sub_Category`;
+DROP TABLE IF EXISTS `Main_Category`;
+
+CREATE TABLE `Main_Category` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `Sub_Category` (
+  `id` INT AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `desc` TEXT NOT NULL,
+  `cost` DECIMAL(10, 2),
+  `main_category_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`main_category_id`) REFERENCES `Main_Category`(`id`)
+);
+
+CREATE TABLE `Customer` (
+  `id` INT AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `sub_category_id` INT,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`sub_category_id`) REFERENCES `Sub_Category`(`id`)
+);
+
+CREATE TABLE `Admin` (
+  `id` INT AUTO_INCREMENT,
+  `username` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `token` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
 INSERT INTO `Main_Category` (`name`) VALUES
 ('Services'),
 ('Contact Us'),
 ('About'),
-('Login'),
 ('Admin Panel');
 
 -- Assuming IDs were assigned in the order the records were inserted
